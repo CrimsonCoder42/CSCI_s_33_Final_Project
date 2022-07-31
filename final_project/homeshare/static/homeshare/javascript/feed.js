b48a1a27c000f63ce3dbcde556c38342a66f3a89
CrimsonCoder42 <nostro37@gmail.com>
2022-07-31 08:51:12 -0400

    separated the feed views

--- final_project/homeshare/static/homeshare/javascript/feed.js
+++ final_project/homeshare/static/homeshare/javascript/feed.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  const homeFeed = document.querySelector('#login-button');
-  const yourProfile = document.querySelector('#register-button');
-  const following = document.querySelector('#login-button');
-  const wishList = document.querySelector('#register-button');
-  const chat = document.querySelector('#login-button');
+  const homeFeed = document.querySelector('#home_feed_bttn').addEventListener('click', () => home_feed() );
+  const yourProfile = document.querySelector('#your_profile_bttn').addEventListener( 'click', () => your_profile() );
+  const following = document.querySelector('#following_bttn').addEventListener( 'click', () => youFollowing() );
+  const wishList = document.querySelector('#wish_list_bttn').addEventListener( 'click', () => yourWishList() );
+  const chat = document.querySelector('#chat_bttn').addEventListener( 'click', () => chatMessaging() );
 
+  document.querySelector('#home_feed').style.display = 'none';
+  document.querySelector('#your_profile').style.display = 'none';
+  document.querySelector('#following').style.display = 'none';
+  document.querySelector('#wish_list').style.display = 'none';
+  document.querySelector('#chat').style.display = 'none';
+  document.querySelector('#property_listing').style.display = 'none';
 
-  youFollowing()
+  home_feed()
 
 });
+
+// All visials separated out.
 
 function home_feed(){
   document.querySelector('#home_feed').style.display = 'block';
@@ -17,14 +25,18 @@ function home_feed(){
   document.querySelector('#following').style.display = 'none';
   document.querySelector('#wish_list').style.display = 'none';
   document.querySelector('#chat').style.display = 'none';
+  document.querySelector('#property_listing').style.display = 'none';
+
 }
 
 function your_profile(){
- document.querySelector('#home_feed').style.display = 'none';
+  document.querySelector('#home_feed').style.display = 'none';
   document.querySelector('#your_profile').style.display = 'block';
   document.querySelector('#following').style.display = 'none';
   document.querySelector('#wish_list').style.display = 'none';
   document.querySelector('#chat').style.display = 'none';
+  document.querySelector('#property_listing').style.display = 'none';
+
 }
 
 function youFollowing(){
@@ -33,20 +45,36 @@ function youFollowing(){
   document.querySelector('#following').style.display = 'block';
   document.querySelector('#wish_list').style.display = 'none';
   document.querySelector('#chat').style.display = 'none';
+  document.querySelector('#property_listing').style.display = 'none';
+
 }
 
-function wishList(){
+function yourWishList(){
   document.querySelector('#home_feed').style.display = 'none';
   document.querySelector('#your_profile').style.display = 'none';
   document.querySelector('#following').style.display = 'none';
   document.querySelector('#wish_list').style.display = 'block';
   document.querySelector('#chat').style.display = 'none';
+  document.querySelector('#property_listing').style.display = 'none';
+
 }
 
-function chat(){
+function chatMessaging(){
   document.querySelector('#home_feed').style.display = 'none';
   document.querySelector('#your_profile').style.display = 'none';
   document.querySelector('#following').style.display = 'none';
   document.querySelector('#wish_list').style.display = 'none';
   document.querySelector('#chat').style.display = 'block';
+  document.querySelector('#property_listing').style.display = 'none';
+
+}
+
+function propertyListing(){
+  document.querySelector('#home_feed').style.display = 'none';
+  document.querySelector('#your_profile').style.display = 'none';
+  document.querySelector('#following').style.display = 'none';
+  document.querySelector('#wish_list').style.display = 'none';
+  document.querySelector('#chat').style.display = 'none';
+  document.querySelector('#property_listing').style.display = 'block';
+
 }
